670b5ae438651160f4d84ec56236dfcdae2b1bc5
PrzyluckiMaciej <mprzylucki02@gmail.com>
2026-01-24 14:06:42 +0100

    feat: added base thymeleaf views

--- src/main/java/mp/teamtask/controller/TaskController.java
+++ src/main/java/mp/teamtask/controller/TaskController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/tasks")
 @RequiredArgsConstructor
 public class TaskController {
 
--- src/main/java/mp/teamtask/repository/UserRepository.java
+++ src/main/java/mp/teamtask/repository/UserRepository.java
@@ -2,12 +2,17 @@ package mp.teamtask.repository;
 
 import mp.teamtask.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.assignedTasks WHERE u.email = :email")
+    Optional<User> findByEmailWithTasks(@Param("email") String email);
+
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
